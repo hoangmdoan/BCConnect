f09b0989e1e59f342e81216e72e5c0fcc08acfe2
TingTing <shaotingwu@gmail.com>
2018-10-27 05:00:04 -0400

    Menu Page change

--- app/src/main/java/com/app/bc/myapplication/ScholarshipActivity.java
+++ app/src/main/java/com/app/bc/myapplication/ScholarshipActivity.java
@@ -1,7 +1,10 @@
 package com.app.bc.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ScholarshipActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class ScholarshipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scholarship);
-    }
-}
+
+        Button button6 =(Button)findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), NextPopUpActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+
+}}
+
