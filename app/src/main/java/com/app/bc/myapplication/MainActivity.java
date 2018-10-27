dec5024582f31cbb5323a3f644ca559e41ff330f
TingTing <shaotingwu@gmail.com>
2018-10-26 22:43:29 -0400

    Menu Page

--- app/src/main/java/com/app/bc/myapplication/MainActivity.java
+++ app/src/main/java/com/app/bc/myapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package com.app.bc.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 =(Button)findViewById(R.id.button1);
+        final EditText editText1 = (EditText)findViewById(R.id.editText1) ;
+        final EditText editText2 = (EditText)findViewById(R.id.editText2);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),MenuPage.class);
+                Editable username = editText1.getText();//startIntent.putExtra("org.mentorschools.quicklauncher.SOMETHING","HELLO WORLD!");
+                startActivity(startIntent);
+            }
+        });
+
+
     }
-    //Ting have comment here kkk6545
+    //Ting have comment here kkk6545final
 }
