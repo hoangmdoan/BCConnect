ae187e3684b0d17ce5795f126032ad05aa499076
Doan <doanhm1@mail.broward.edu>
2018-10-26 20:24:49 -0400

    TingTing here

--- app/src/main/java/com/app/bc/myapplication/MainActivity.java
+++ app/src/main/java/com/app/bc/myapplication/MainActivity.java
@@ -10,5 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //Ting have comment here
+    //Ting have comment here kkk
 }
