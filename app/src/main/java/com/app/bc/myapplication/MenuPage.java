33b358fd5f234551fe768a57d55415412f50a74e
Doan <doanhm1@mail.broward.edu>
2018-10-26 23:14:57 -0400

    Homework Activity and Message Activity

--- app/src/main/java/com/app/bc/myapplication/MenuPage.java
+++ app/src/main/java/com/app/bc/myapplication/MenuPage.java
@@ -1,7 +1,12 @@
 package com.app.bc.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MenuPage extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MenuPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_page);
+
+        Button button1 =(Button)findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),HomeworkActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
